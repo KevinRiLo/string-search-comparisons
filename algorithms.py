dc04f69cc10206c6b04c15d6d7ff5e5c91850fc8
Kevin <Kevinrl321@icloud.com>
2025-11-01 15:37:48 -0400

    Implemented the KMP algorithm and added another very large txt file for algorithm testing

--- algorithms.py
+++ algorithms.py
@@ -1,10 +1,10 @@
-def naive(text, pattern):
-    t = list(text)
-    p = list(pattern)
+def naive(text, pattern): # naive search algorithm
+    t = list(text) # convert string to a list of characters
+    p = list(pattern) # convert pattern to a list of characters
     result = False
     for i in range(len(t) - len(p)):
         for j in range(len(p)):
-            if t[i+j] != p[j]:
+            if t[i+j] != p[j]: # check is pattern matches current index of string
                 result = False
                 break
             else:
@@ -12,3 +12,50 @@ def naive(text, pattern):
         if result:
             return result
     return result
+
+
+def lps_array(pattern): # Algorithm for getting the LPS array used in KMP
+    m = len(pattern)
+    lps = [0] * m
+
+    length = 0
+    i = 1
+
+    while i < m:
+        if pattern[i] == pattern[length]:
+            length +=1
+            lps[i] = length
+            i+=1
+        else:
+            if length != 0:
+                length = lps[length-1]
+            else:
+                lps[i] = 0
+                i+=1
+    return lps
+
+
+
+def kmp(text, pattern): # KMP search algorithm
+    lps = lps_array(pattern)
+
+    i = 0  
+    j = 0  
+    result = []
+
+    while i < len(text):
+        if text[i] == pattern[j]:
+            i += 1
+            j += 1
+
+            if j == len(pattern):
+                result.append(i - j)
+                j = lps[j - 1]
+        else:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+    
+    return result
+    
--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 import algorithms
 import time
 
-def multi_naive(text, pattern):
+def multi_naive(text, pattern): # Method runs naive search with multiple patterns
     start_time = time.time()
     time.perf_counter()
     for i in range(len(pattern)):
@@ -12,7 +12,19 @@ def multi_naive(text, pattern):
     total_time = time.time() - start_time
     print(f"Run time of algorithm: {total_time:.6f} seconds")
 
-patterns = ["hi", "hello", "Hello", "how are you", "Tell me", "Muse", "was now gone"]
-with open("large.txt", "r") as file:
+def multi_kmp(text, pattern): # Method runs KMP search with multiple patterns
+    start_time = time.time()
+    time.perf_counter()
+    for i in range(len(pattern)):
+        if algorithms.kmp(text, pattern[i]):
+            print(f"'{pattern[i]}' found in text")
+        else:
+            print(f"'{pattern[i]}' not found in text")
+    total_time = time.time() - start_time
+    print(f"Run time of algorithm: {total_time:.6f} seconds")
+
+patterns = ["hi", "hello", "Hello", "how are you", "Tell me", "Muse", "was now gone", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbba"]
+with open("odyssey.txt", "r") as file:
     test_string = file.read()
 multi_naive(test_string, patterns)
+multi_kmp(test_string, patterns)
