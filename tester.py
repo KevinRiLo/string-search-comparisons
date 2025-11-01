edcdfcc120a736708b8e4daecba9432801f9e663
Kevin <Kevinrl321@icloud.com>
2025-11-01 14:29:12 -0400

    Created large txt file to test algorithms with

--- tester.py
+++ tester.py
@@ -1,13 +1,18 @@
 import algorithms
+import time
 
 def multi_naive(text, pattern):
+    start_time = time.time()
+    time.perf_counter()
     for i in range(len(pattern)):
         if algorithms.naive(text, pattern[i]):
-            print(f"{pattern[i]} found in text")
+            print(f"'{pattern[i]}' found in text")
         else:
-            print(f"{pattern[i]} not found in text")
+            print(f"'{pattern[i]}' not found in text")
+    total_time = time.time() - start_time
+    print(f"Run time of algorithm: {total_time:.6f} seconds")
 
 patterns = ["hi", "hello", "Hello", "how are you", "Tell me", "Muse", "was now gone"]
-with open("Odyssey_Excerpt.txt", "r") as file:
+with open("large.txt", "r") as file:
     test_string = file.read()
 multi_naive(test_string, patterns)
