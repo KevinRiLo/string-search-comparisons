9744f1e11e4ea27e801d9cdb2ad481a6c9a9b322
Kevin <Kevinrl321@icloud.com>
2025-11-13 14:06:41 -0500

    Implemented Boyer-Moore search algorithm

--- algorithms.py
+++ algorithms.py
@@ -4,7 +4,7 @@ def naive(text, pattern): # naive search algorithm
     result = False
     for i in range(len(t) - len(p)):
         for j in range(len(p)):
-            if t[i+j] != p[j]: # check is pattern matches current index of string
+            if t[i+j] != p[j]: # check if pattern matches current index of string
                 result = False
                 break
             else:
@@ -37,7 +37,9 @@ def lps_array(pattern): # Algorithm for getting the LPS array used in KMP
 
 
 def kmp(text, pattern): # KMP search algorithm
-    lps = lps_array(pattern)
+    text = list(text) # Convert text to character list
+    pattern = list(pattern) # Convert pattern to character list
+    lps = lps_array(pattern) # Fill out LPS array 
 
     i = 0  
     j = 0  
@@ -57,5 +59,33 @@ def kmp(text, pattern): # KMP search algorithm
             else:
                 i += 1
     
-    return result
+    return result #This returns all indexes of matches
+
+def bad_character_heuristic(pattern): # Algorithm fills out bad character table for use in Boyer_Moore algorithm
+    m = len(pattern) # m = length of pattern
+    bc_table = {}
+    for i in range(127): # Loop through all ascii values
+        bc_table.update({chr(i): m}) # Fill out all values with m
+    for i in range(m):
+        bc_table.update({pattern[i]: (m - i - 1)}) # Update values of the characters in the pattern with their rightmost index
     
+    return bc_table
+
+def boyer_moore(text, pattern): # Boyer-Moore search algorithm
+    t = list(text)  # Convert text to char list
+    p = list(pattern)   # Convert pattern to char list
+    n = len(t) # n = length of t
+    m = len(p) # m = length of p
+    bc_table = bad_character_heuristic(p) # Initialize B-C table
+
+    i = m-1
+    while i < n:
+        j = m-1
+        while j>= 0 and p[j] == t[i]:
+            i = i-1
+            j = j-1
+        if j == -1:
+            return True # Match found
+        i = i + max(bc_table.get(t[i]), m-j)
+    return False    # No match found in entire text
+
--- tester.py
+++ tester.py
@@ -10,7 +10,7 @@ def multi_naive(text, pattern): # Method runs naive search with multiple pattern
         else:
             print(f"'{pattern[i]}' not found in text")
     total_time = time.time() - start_time
-    print(f"Run time of algorithm: {total_time:.6f} seconds")
+    print(f"Run time of naive algorithm: {total_time:.6f} seconds")
 
 def multi_kmp(text, pattern): # Method runs KMP search with multiple patterns
     start_time = time.time()
@@ -21,10 +21,22 @@ def multi_kmp(text, pattern): # Method runs KMP search with multiple patterns
         else:
             print(f"'{pattern[i]}' not found in text")
     total_time = time.time() - start_time
-    print(f"Run time of algorithm: {total_time:.6f} seconds")
+    print(f"Run time of KMP algorithm: {total_time:.6f} seconds")
 
-patterns = ["hi", "hello", "Hello", "how are you", "Tell me", "Muse", "was now gone", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbba"]
+def multi_boyer_moore(text, pattern): # Method runs Boyer-Moore search with multiple patterns
+    start_time = time.time()
+    time.perf_counter()
+    for i in range(len(pattern)):
+        if algorithms.boyer_moore(text, pattern[i]):
+            print(f"'{pattern[i]}' found in text")
+        else:
+            print(f"'{pattern[i]}' not found in text")
+    total_time = time.time() - start_time
+    print(f"Run time of Boyer-Moore algorithm: {total_time:.6f} seconds")
+
+patterns = ["hi", "hello", "Hello", "how are you", "Tell me", "Muse", "was now gone"]
 with open("odyssey.txt", "r") as file:
     test_string = file.read()
 multi_naive(test_string, patterns)
 multi_kmp(test_string, patterns)
+multi_boyer_moore(test_string, patterns)
