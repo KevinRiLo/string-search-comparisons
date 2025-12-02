7cb76e3f42077f5a401ceb84d233c638d3e8a30e
Kevin <Kevinrl321@icloud.com>
2025-12-02 14:16:02 -0500

    Changed plot to better visualize the comparison between each algorithm with different pattern lengths.

--- tester.py
+++ tester.py
@@ -7,6 +7,11 @@ import matplotlib.pyplot as plt
 # At the end of the testing, this program creates and shows a plot of the results.
 
 
+# TODO: Edit each algorithm to return the runtime after everysingle pattern so the 
+# plot will be more accurate.
+
+
+
 def multi_naive(text, pattern): # Method runs naive search with multiple patterns
     start_time = time.time()
     time.perf_counter()
@@ -49,10 +54,10 @@ patterns = []
 single_pattern = ["Then Jove's daughter Minerva came up to them, having assumed the form and voice of Mentor."]
 # single_pattern = ["Hello"]
 # Plot values
-x = ["Naive", "KMP", "Boyer-Moore"]
-y = []
-z = []
-
+x = [1, 10, 100, 300, 600, 1000]
+y1 = []
+y2 = []
+y3 = []
 
 with open("odyssey.txt", "r") as file:
     test_string = file.read().replace('\n', ' ') # Reads file and replaces new lines with a space character
@@ -63,26 +68,38 @@ with open("words.txt", "r") as file:
     patterns = content.split()
     file.close()
 
-print("\n\nMulti pattern testing:\n\n")
-y.append(multi_naive(test_string, patterns))
-y.append(multi_kmp(test_string, patterns))
-y.append(multi_boyer_moore(test_string, patterns))
+print("\n\nNaive algorithm testing:\n\n")
+y1.append(multi_naive(test_string, single_pattern))
+y1.append(multi_naive(test_string, patterns[:10]))
+y1.append(multi_naive(test_string, patterns[:100]))
+y1.append(multi_naive(test_string, patterns[:300]))
+y1.append(multi_naive(test_string, patterns[:600]))
+y1.append(multi_naive(test_string, patterns))
 
-print("\n\nSingle pattern testing:\n\n")
-z.append(multi_naive(test_string, single_pattern))
-z.append(multi_kmp(test_string, single_pattern))
-z.append(multi_boyer_moore(test_string, single_pattern))
 
-# Plot the runtimes of the algorithms when using multiple patterns
-plt.bar(x, y)
-plt.xlabel("Algorithm")
+print("\n\nKMP algorithm testing:\n\n")
+y2.append(multi_kmp(test_string, single_pattern))
+y2.append(multi_kmp(test_string, patterns[:10]))
+y2.append(multi_kmp(test_string, patterns[:100]))
+y2.append(multi_kmp(test_string, patterns[:300]))
+y2.append(multi_kmp(test_string, patterns[:600]))
+y2.append(multi_kmp(test_string, patterns))
+
+
+print("\n\nBoyer-Moore algorithm testing:\n\n")
+y3.append(multi_boyer_moore(test_string, single_pattern))
+y3.append(multi_boyer_moore(test_string, patterns[:10]))
+y3.append(multi_boyer_moore(test_string, patterns[:100]))
+y3.append(multi_boyer_moore(test_string, patterns[:300]))
+y3.append(multi_boyer_moore(test_string, patterns[:600]))
+y3.append(multi_boyer_moore(test_string, patterns))
+
+plt.plot(x,y1, label="Naive", marker='o')
+plt.plot(x,y2, label="KMP", marker='o')
+plt.plot(x,y3, label="Boyer-Moore", marker='o')
+plt.xlabel("Number of Patterns")
 plt.ylabel("Runtime (sec)")
-plt.title("Multi-Pattern Runtimes")
+plt.title("Algorithm Runtimes Over Different Numbers of Patterns")
 plt.show()
 
-# Plot the runtimes of the algorithms when using one pattern
-plt.bar(x, z)
-plt.xlabel("Algorithm")
-plt.ylabel("Runtime (sec)")
-plt.title("Single-Pattern Runtimes")
-plt.show()
+
